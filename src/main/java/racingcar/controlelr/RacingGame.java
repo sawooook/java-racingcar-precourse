b47d4f8179abd60def011dfa6421395adc614c98
sawook <lna1003@naver.com>
2022-10-06 20:56:56 +0900

    feat: 자동차의 이름을 받을 수 있는 기능 추가
    자동차의 이름을 받을 수 있도록 하는 기능 추가

---
+++ src/main/java/racingcar/controlelr/RacingGame.java
@@ -0,0 +1,17 @@
+package racingcar.controlelr;
+
+
+import racingcar.common.SplitUtil;
+import racingcar.model.Car;
+import racingcar.model.Participate;
+import racingcar.model.RacingCarList;
+import racingcar.view.InputView;
+
+import java.util.List;
+
+public class RacingGame {
+
+    public void start() {
+        String carName = new InputView().getCarName();
+    }
+}
