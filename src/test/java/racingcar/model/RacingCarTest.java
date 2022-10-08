bcdc28d2b40160066813da018fe78a6164de8308
sawook <lna1003@naver.com>
2022-10-08 21:04:37 +0900

    test: 경기 결과를 받는 테스트 코드추가
    - Mock 구현체를 통하여 랜덤 숫자를 테스트 할 수 있도록 기능 개발
    - 경기 결과 및 이동거리를 측정 할 수 있는 테스트 코드 추가

--- src/test/java/racingcar/model/RacingCarTest.java
+++ src/test/java/racingcar/model/RacingCarTest.java
@@ -2,6 +2,7 @@ package racingcar.model;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.common.strategy.MockStrategy;
 import racingcar.model.dto.RacingResult;
 
 import java.util.Arrays;
@@ -29,5 +30,31 @@ class RacingCarTest {
         assertEquals(result.get(0).getMove(), moveCountA);
         assertEquals(result.get(1).getName(), testB);
         assertEquals(result.get(1).getMove(), moveCountB);
+    }
+
+    @Test
+    @DisplayName("숫자가 4를 넘으면 움직이도록 한다.")
+    void moveNumberUpFive() {
+        String testA = "testA";
+        int moveCountA = 1;
+
+        RacingCar participate = new RacingCar(Arrays.asList(Car.participate(testA, moveCountA)));
+        participate.start(new MockStrategy(4));
+
+        assertEquals(participate.getCars().get(0).getName(), "testA");
+        assertEquals(participate.getCars().get(0).getMove(), 2);
+    }
+
+    @Test
+    @DisplayName("숫자가 4를 넘지 않으면 멈춘다.")
+    void stopNumberUnderFive() {
+        String testA = "testA";
+        int moveCountA = 1;
+
+        RacingCar participate = new RacingCar(Arrays.asList(Car.participate(testA, moveCountA)));
+        participate.start(new MockStrategy(3));
+
+        assertEquals(participate.getCars().get(0).getName(), "testA");
+        assertEquals(participate.getCars().get(0).getMove(), 1);
     }
 }
